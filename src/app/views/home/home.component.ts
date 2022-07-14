e0356c50c935c12a7db68ba2fd79cd4b0a393c66
LuizGabriel2000 <luizgabriel.lg2000@outlook.com>
2022-07-14 14:20:44 -0300

    Implementada a estrutura sobre os carros a combustão , carros eletricos e carros Hibridos

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,13 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'; 
+
+
+let oi: string = "";
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
+
+
 export class HomeComponent implements OnInit {
 
   constructor() { }
+
+  btn() { 
+    console.log('arli carai', oi)
+    oi = "qualquer coisa"
+  }
 
   ngOnInit(): void {
   }
